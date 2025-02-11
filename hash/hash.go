c92836d818da43da1436379d8e0967cd661f2ed3
Dmitrii <dimapevtsov02@gmail.com>
2025-02-11 15:10:38 +0300

    Add Error handling

--- hash/hash.go
+++ hash/hash.go
@@ -7,11 +7,15 @@ type Node struct {
 	next, prev *Node
 }
 
+// Создание нового элемента таблицы
 func NewNode(key int) *Node {
 	return &Node{key, nil, nil}
 }
 
 func (n *Node) Key() int {
+	if n == nil {
+		panic("Nill pointer in *Node.Key()")
+	}
 	return n.key
 }
 
@@ -20,15 +24,38 @@ type Table struct {
 	h    func(int) int
 }
 
+// Создание новой таблицы с закрытой адресацией
 func NewTable(len int, h func(int) int) *Table {
+	if h == nil {
+		panic("Nill hash func in NewTable()")
+	}
 	return &Table{make([]*Node, len), h}
 }
 
+// Создаёт простую хэш функцию с методом деления
 func ModFunc(len int) func(int) int {
+	if len < 1 {
+		panic("Length is less than 1 in ModFunc()")
+	}
 	return func(key int) int { return key % len }
 }
 
+// Создаёт хэш функцию с методом умножения
+func MultFunc(len int) func(int) int {
+	if len < 1 {
+		panic("Length is less than 1 in MultFunc()")
+	}
+	const gr = 1.0 / 1.6180339887
+	return func(key int) int {
+		return int(float64(len) * (float64(key)*gr - float64(int(float64(key)*gr))))
+	}
+}
+
+// Вставка элемента
 func (t *Table) Insert(n *Node) {
+	if n == nil {
+		return
+	}
 	next := t.data[t.h(n.key)]
 	t.data[t.h(n.key)] = n
 	n.next = next
@@ -37,6 +64,7 @@ func (t *Table) Insert(n *Node) {
 	}
 }
 
+// Поиск по ключу
 func (t *Table) Search(key int) *Node {
 	cur := t.data[t.h(key)]
 	for cur != nil {
@@ -48,7 +76,11 @@ func (t *Table) Search(key int) *Node {
 	return cur
 }
 
+// Удаление элемента
 func (t *Table) Delete(n *Node) {
+	if n == nil {
+		return
+	}
 	if n.prev != nil {
 		n.prev.next = n.next
 		if n.next != nil {
@@ -59,6 +91,7 @@ func (t *Table) Delete(n *Node) {
 	}
 }
 
+// Вывод таблицы по хэшам
 func (t *Table) Print() {
 	for i, str := range t.data {
 		fmt.Printf("%d: ", i)
