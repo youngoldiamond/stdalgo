51639783ba538115333ed1fc5ee164e3b4a6fe9e
Dmitrii <dimapevtsov02@gmail.com>
2025-02-11 23:17:30 +0300

    Change structure

--- hash/closedHash.go
+++ hash/closedHash.go
@@ -2,7 +2,6 @@ package hash
 
 import (
 	"fmt"
-	"math/rand/v2"
 )
 
 type Node struct {
@@ -36,40 +35,6 @@ func NewTable(len int, h func(int) int) *ClosedTable {
 		panic("Length is less than 1 in NewTable()")
 	}
 	return &ClosedTable{make([]*Node, len), h}
-}
-
-// Создаёт простую хэш функцию с методом деления
-func ModFunc(len int) func(int) int {
-	if len < 1 {
-		panic("Length is less than 1 in ModFunc()")
-	}
-	return func(key int) int { return key % len }
-}
-
-// Создаёт хэш функцию с методом умножения
-func MultFunc(len int) func(int) int {
-	if len < 1 {
-		panic("Length is less than 1 in MultFunc()")
-	}
-	const gr = 0.618033989
-	return func(key int) int {
-		return int(float64(len) * (float64(key)*gr - float64(int(float64(key)*gr))))
-	}
-}
-
-// Создаёт функцию из универсального множества хэш функций с ключами до 1^31
-func UniFunc(len int) func(int) int {
-	if len < 1 {
-		panic("Length is less than 1 in UniFunc()")
-	}
-	const maxInt32 = (1 << 31) - 1
-	a := rand.Int64N(maxInt32-1) + 1
-	b := a
-	for a == b {
-		b = rand.Int64N(maxInt32)
-	}
-	fmt.Printf("a = %d, b = %d\n", a, b)
-	return func(key int) int { return int((a*int64(key)+b)%(maxInt32+1)) % len }
 }
 
 // Вставка элемента
